bce79e23fc629b34fa99a2dc40402d45bfdf6097
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-12-06 12:23:22 +0100

    Refactors GamecenterApplication to GameCenterApplication.

--- src/main/java/nl/hsleiden/gamecenter/GameCenterApplication.java
+++ src/main/java/nl/hsleiden/gamecenter/GameCenterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GamecenterApplication {
+public class GameCenterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GamecenterApplication.class, args);
+		SpringApplication.run(GameCenterApplication.class, args);
 	}
 
 }
