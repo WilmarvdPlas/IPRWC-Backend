571794a21054710b3725615863c42f0f028f9646
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2023-01-10 15:42:34 +0100

    Adds JWTFilter with doFilterInternal() method to project.

---
+++ src/main/java/nl/hsleiden/gamecenter/security/JWTFilter.java
@@ -0,0 +1,56 @@
+package nl.hsleiden.gamecenter.security;
+
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import nl.hsleiden.gamecenter.services.LoggedInUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JWTFilter extends OncePerRequestFilter {
+
+    private final LoggedInUserDetailsService loggedInUserDetailsService;
+    private final JWTUtil jwtUtil;
+
+    @Autowired
+    public JWTFilter(LoggedInUserDetailsService loggedInUserDetailsService, JWTUtil jwtUtil) {
+        this.loggedInUserDetailsService = loggedInUserDetailsService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        if(authHeader != null && !authHeader.isBlank() && authHeader.startsWith("Bearer ")){
+            String jwt = authHeader.substring(7);
+            if(jwt == null || jwt.isBlank()){
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid JWT Token in Bearer Header");
+            }else {
+                try{
+                    String email = jwtUtil.validateTokenAndRetrieveSubject(jwt);
+                    UserDetails userDetails = loggedInUserDetailsService.loadUserByUsername(email);
+                    UsernamePasswordAuthenticationToken authToken =
+                            new UsernamePasswordAuthenticationToken(email, userDetails.getPassword(), userDetails.getAuthorities());
+                    if(SecurityContextHolder.getContext().getAuthentication() == null){
+                        SecurityContextHolder.getContext().setAuthentication(authToken);
+                    }
+                }catch(JWTVerificationException exc){
+                    response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid JWT Token");
+                }
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
