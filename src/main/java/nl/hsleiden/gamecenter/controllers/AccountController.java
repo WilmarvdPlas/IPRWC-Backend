130886f8e7073d6f575db8af1ac972066a3b907b
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2023-01-10 15:47:20 +0100

    Adds createAccount() and getAccount() methods to AccountController.

--- src/main/java/nl/hsleiden/gamecenter/controllers/AccountController.java
+++ src/main/java/nl/hsleiden/gamecenter/controllers/AccountController.java
@@ -3,26 +3,56 @@ package nl.hsleiden.gamecenter.controllers;
 import nl.hsleiden.gamecenter.DAOs.AccountDAO;
 import nl.hsleiden.gamecenter.models.Account;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "api/account")
 public class AccountController {
 
     private final AccountDAO accountDAO;
+    private final PasswordEncoder passwordEncoder;
+
 
     @Autowired
-    public AccountController(AccountDAO accountDAO) {
+    public AccountController(AccountDAO accountDAO, PasswordEncoder passwordEncoder) {
         this.accountDAO = accountDAO;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping
     public List<Account> getAccounts() {
         return accountDAO.getAccounts();
+    }
+
+    @GetMapping(path = "{accountId}")
+    public Optional<Account> getAccount(@PathVariable("accountId") UUID id) {
+        return accountDAO.getAccount(id);
+    }
+
+    @PostMapping(path = "register")
+    public ResponseEntity createAccount(@RequestBody Account account) {
+
+        if (accountDAO.findAccountByEmail(account.getEmail()).isPresent()) {
+            return new ResponseEntity<>("Email address is already in use", HttpStatus.CONFLICT);
+        }
+
+        if (account.getAdministrator()) {
+            return new ResponseEntity<>("Illegal registration", HttpStatus.UNAUTHORIZED);
+        }
+
+        account.setPassword(passwordEncoder.encode(account.getPassword()));
+        accountDAO.createAccount(account);
+
+        return new ResponseEntity<>("Success", HttpStatus.CREATED);
+
     }
 
 }
