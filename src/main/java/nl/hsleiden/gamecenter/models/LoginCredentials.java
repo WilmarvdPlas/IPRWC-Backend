4f154db21592cad1cfc3cdc204d7efeef619486e
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2023-01-10 15:39:37 +0100

    Adds LoginCredentials model to project.

---
+++ src/main/java/nl/hsleiden/gamecenter/models/LoginCredentials.java
@@ -0,0 +1,13 @@
+package nl.hsleiden.gamecenter.models;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class LoginCredentials {
+        private String email;
+        private String password;
+}
