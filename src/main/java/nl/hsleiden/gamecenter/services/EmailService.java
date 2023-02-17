523d0cdb1b9260de0f7a4539e13f0b8d6d04538f
Wilmar <s1137070@student.hsleiden.nl>
2023-02-17 13:17:47 +0100

    Improves EmailService.

--- src/main/java/nl/hsleiden/gamecenter/services/EmailService.java
+++ src/main/java/nl/hsleiden/gamecenter/services/EmailService.java
@@ -1,47 +1,67 @@
 package nl.hsleiden.gamecenter.services;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
+@Component
 public class EmailService {
 
-    /**
-     * Sends a given new password to a given email.
-     * @param email Email where the new password will be sent to.
-     * @param newPassword Password that will be sent.
-     */
-    public static void sendPassword(String email, String newPassword) {
-        final String email_username = "noreply.spine.ngo@gmail.com";
-        final String email_password = "rbuxgkumcuqvzwfp";
+    @Value("${email_address}")
+    private String email;
 
-        Properties prop = new Properties();
-        prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.smtp.port", "587");
-        prop.put("mail.smtp.auth", "true");
-        prop.put("mail.smtp.starttls.enable", "true");
+    @Value("${email_password}")
+    private String password;
 
-        Session session = Session.getInstance(prop,
-                new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(email_username, email_password);
-                    }
-                });
+    private void sendEmail(String recipientEmail, String messageSubject, String messageText) {
+        Properties properties = createProperties();
+        Session session = createSession(properties, email, password);
 
         try {
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("from@gmail.com"));
-            message.setRecipients(
-                    Message.RecipientType.TO,
-                    InternetAddress.parse(email)
-            );
-            message.setSubject("Uw nieuwe Spine-account wachtwoord");
-            message.setText("Beste gebruiker, \n\n" + "Uw nieuwe wachtwoord is: " + newPassword + "\n\n" + "Met vriendelijke groet,\n" + "Het Spine-team");
-
-            Transport.send(message);
+            Transport.send(createMessage(session, recipientEmail, messageSubject, messageText));
         } catch (MessagingException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
+    }
+
+    private Properties createProperties() {
+
+        Properties properties = new Properties();
+
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+
+        return properties;
+    }
+
+    private Session createSession(Properties properties, String email, String password) {
+        Session session = Session.getInstance(
+                properties,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(email, password);
+                    }
+                });
+        return session;
+    }
+
+    private Message createMessage(Session session, String recipientEmail, String messageSubject, String messageText) throws MessagingException {
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress("from@gmail.com"));
+
+        message.setRecipients(
+                Message.RecipientType.TO,
+                InternetAddress.parse(recipientEmail)
+        );
+        message.setSubject(messageSubject);
+        message.setText("Beste gebruiker, \n\n" + messageText + "\n\n" + "Met vriendelijke groet,\n" + "Het Spine-team");
+
+        return message;
     }
 }
