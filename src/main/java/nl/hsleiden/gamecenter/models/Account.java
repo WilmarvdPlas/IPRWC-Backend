5dd53d4811845337788e40e510d898f0afadb1b1
Wilmar <s1137070@student.hsleiden.nl>
2023-02-17 13:53:46 +0100

    Adds OneToMany relations and CreationTimestamp

--- src/main/java/nl/hsleiden/gamecenter/models/Account.java
+++ src/main/java/nl/hsleiden/gamecenter/models/Account.java
@@ -3,8 +3,11 @@ package nl.hsleiden.gamecenter.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -29,5 +32,18 @@ public class Account {
 
     @Column
     private Boolean administrator;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "account")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private List<CartProduct> cartProducts;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "account")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private List<Order> orders;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.DATE)
+    @Column(name = "date_created")
+    private Date dateCreated;
 
 }
