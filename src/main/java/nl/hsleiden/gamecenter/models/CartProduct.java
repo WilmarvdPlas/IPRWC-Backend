ea47544a5b7a178f2eb7fc4624e11e3a47e77d37
Wilmar <s1137070@student.hsleiden.nl>
2023-02-17 13:54:59 +0100

    Adds CreationTimestamp to CartProduct.

--- src/main/java/nl/hsleiden/gamecenter/models/CartProduct.java
+++ src/main/java/nl/hsleiden/gamecenter/models/CartProduct.java
@@ -2,9 +2,11 @@ package nl.hsleiden.gamecenter.models;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 
+import java.util.Date;
 import java.util.UUID;
 
 @Entity
@@ -25,5 +27,9 @@ public class CartProduct {
     @JoinColumn(name = "account", referencedColumnName = "id", nullable = false)
     private Account account;
 
-}
+    @CreationTimestamp
+    @Temporal(TemporalType.DATE)
+    @Column(name = "date_created")
+    private Date dateCreated;
 
+}
