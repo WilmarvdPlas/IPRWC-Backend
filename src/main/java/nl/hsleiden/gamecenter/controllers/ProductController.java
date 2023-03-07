fb83ba4e99dbede5db044cfef09051afc579d191
Wilmar <s1137070@student.hsleiden.nl>
2023-03-07 20:53:07 +0100

    Add createProduct method to ProductController.

--- src/main/java/nl/hsleiden/gamecenter/controllers/ProductController.java
+++ src/main/java/nl/hsleiden/gamecenter/controllers/ProductController.java
@@ -3,13 +3,9 @@ package nl.hsleiden.gamecenter.controllers;
 import nl.hsleiden.gamecenter.DAOs.ProductDAO;
 import nl.hsleiden.gamecenter.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/product")
@@ -27,5 +23,5 @@ public class ProductController {
         this.productDAO.createProduct(product);
         return new ResponseEntity(HttpStatus.CREATED);
     }
-
+    
 }
