1aa59b2c578dc714850d3748c9fa605975aef77f
Wilmar <s1137070@student.hsleiden.nl>
2023-03-10 16:45:33 +0100

    Add createCartProduct endpoint.

--- src/main/java/nl/hsleiden/gamecenter/DAOs/CartProductDAO.java
+++ src/main/java/nl/hsleiden/gamecenter/DAOs/CartProductDAO.java
@@ -1,5 +1,6 @@
 package nl.hsleiden.gamecenter.DAOs;
 
+import nl.hsleiden.gamecenter.models.CartProduct;
 import nl.hsleiden.gamecenter.repositories.CartProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,10 @@ public class CartProductDAO {
     @Autowired
     public CartProductDAO(CartProductRepository repository) {
         this.repository = repository;
+    }
+
+    public void createCartProduct(CartProduct cartProduct) {
+        repository.save(cartProduct);
     }
 
 }
--- src/main/java/nl/hsleiden/gamecenter/controllers/CartProductController.java
+++ src/main/java/nl/hsleiden/gamecenter/controllers/CartProductController.java
@@ -1,7 +1,12 @@
 package nl.hsleiden.gamecenter.controllers;
 
 import nl.hsleiden.gamecenter.DAOs.CartProductDAO;
+import nl.hsleiden.gamecenter.models.CartProduct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +19,12 @@ public class CartProductController {
     @Autowired
     public CartProductController(CartProductDAO cartProductDAO) {
         this.cartProductDAO = cartProductDAO;
+    }
+
+    @PostMapping
+    public ResponseEntity createCartProduct(@RequestBody CartProduct cartProduct) {
+        this.cartProductDAO.createCartProduct(cartProduct);
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
