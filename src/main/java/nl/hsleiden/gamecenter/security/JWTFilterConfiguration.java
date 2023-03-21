56abdd4f50400cbba21d0ac65e673bc295f58d62
Wilmar <s1137070@student.hsleiden.nl>
2023-03-21 16:13:54 +0100

    Change requestMatchers.

--- src/main/java/nl/hsleiden/gamecenter/security/JWTFilterConfiguration.java
+++ src/main/java/nl/hsleiden/gamecenter/security/JWTFilterConfiguration.java
@@ -24,26 +24,24 @@ public class JWTFilterConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeHttpRequests()
                 // ALL ALLOWED ENDPOINTS FOR ALL USERS (AUTHENTICATED AND UNAUTHENTICATED)
-                .requestMatchers("/api/auth/**").permitAll()
-                .requestMatchers("/api/account/register").permitAll()
+                .requestMatchers("/api/auth/**", "/api/account/register", "/api/product/{id}").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/product").permitAll()
-                .requestMatchers("/api/product/{id}").permitAll()
                 // USER AND ADMIN ROUTES
-                .requestMatchers("/api/cart_product/{id}/buy_product").hasAnyRole("USER", "ADMINISTRATOR")
-                .requestMatchers("/api/account/{id}").hasAnyRole("USER", "ADMINISTRATOR")
-                .requestMatchers("/api/account/{id}/change_password").hasAnyRole("USER", "ADMINISTRATOR")
-                .requestMatchers("/api/cart_product").hasAnyRole("USER", "ADMINISTRATOR")
-                .requestMatchers("/api/cart_product/account={id}").hasAnyRole("USER", "ADMINISTRATOR")
-                .requestMatchers("/api/cart_product/{id}/update_count").hasAnyRole("USER", "ADMINISTRATOR")
-                .requestMatchers("/api/cart_product/{id}").hasAnyRole("USER", "ADMINISTRATOR")
+                .requestMatchers(
+                        "/api/cart_product/transfer_cart",
+                        "/api/cart_product/{id}/buy_product",
+                        "/api/account/{id}",
+                        "/api/account/{id}/change_password",
+                        "/api/cart_product",
+                        "/api/cart_product/account={id}",
+                        "/api/cart_product/{id}/update_count",
+                        "/api/cart_product/{id}",
+                        "/api/transaction/account={accountId}",
+                        "/api/transaction_product/transaction={transactionId}",
+                        "/api/transaction_product").hasAnyRole("USER", "ADMINISTRATOR")
                 .requestMatchers(HttpMethod.POST, "/api/transaction").hasAnyRole("USER", "ADMINISTRATOR")
-                .requestMatchers("/api/transaction/account={accountId}").hasAnyRole("USER", "ADMINISTRATOR")
-                .requestMatchers("/api/transaction_product/transaction={transactionId}").hasAnyRole("USER", "ADMINISTRATOR")
-                .requestMatchers("/api/transaction_product").hasAnyRole("USER", "ADMINISTRATOR")
                 // ADMIN ROUTES
-                .requestMatchers("/api/account").hasRole("ADMINISTRATOR")
-                .requestMatchers("/api/account/{id}/administrator").hasRole("ADMINISTRATOR")
-                .requestMatchers("/api/product/{id}/edit_stock").hasRole("ADMINISTRATOR")
+                .requestMatchers("/api/account", "/api/account/{id}/administrator", "/api/product/{id}/edit_stock").hasRole("ADMINISTRATOR")
                 .requestMatchers(HttpMethod.POST, "/api/product").hasRole("ADMINISTRATOR")
                 .requestMatchers(HttpMethod.GET, "/api/transaction").hasRole("ADMINISTRATOR")
                 .and()
